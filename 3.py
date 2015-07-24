9b76a628a5dd28cdf24cb9f9f492f00d1db7b306
borrellim <tiaborrelli@gmail.com>
2015-07-24 21:42:54 +0200

    Partial Word Searching
    Write a method that will search an array of strings for all strings that
    contain another string, ignoring capitalization. Then return an array of
    the found strings.

---
+++ 3.py
@@ -0,0 +1,11 @@
+__author__ = 'Mattia'
+
+def is_divisible(n,x,y):
+    if (n%x == 0 and n%y == 0):
+        print True
+        return True
+    else:
+        print False
+        return False
+is_divisible(3,1,3)
+
